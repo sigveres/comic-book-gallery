71d2617ff7ff6ccdc9dd543b221afa174f44db21
Sigve Resaland <sigve.res@gmail.com>
2017-11-15 11:14:58 +0100

    Added comic books detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller");
+            return View();
         }
-
     }
 }
