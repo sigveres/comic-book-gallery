e3fcd1232578d10262f359ddf141986a420c2006
Sigve Resaland <sigve.res@gmail.com>
2017-11-15 01:23:38 +0100

    added detail action method

--- src/ComicBookGallery/Controllers/ComicBokksController.cs
+++ src/ComicBookGallery/Controllers/ComicBokksController.cs
@@ -8,5 +8,10 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBokksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
+
     }
 }
