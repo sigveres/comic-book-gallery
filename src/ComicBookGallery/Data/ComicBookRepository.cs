82ded160282f21c0173f1be2028910bf35f05ee7
Sigve Resaland <sigve.res@gmail.com>
2017-12-04 20:02:28 +0100

    updated comic books controller to use repo

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -11,6 +11,7 @@ namespace ComicBookGallery.Data
         {
             new ComicBook()
             {
+                Id= 1,
                 SeriesTitle = "the amazing spider man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
@@ -27,6 +28,7 @@ namespace ComicBookGallery.Data
             },
     new ComicBook()
     {
+        Id= 2,
         SeriesTitle = "Bone",
         IssueNumber = 50,
         DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
